76019e7d9381720bc8ebe42624d473389aa040f9
paulo <paulo@192.168.0.13>
2022-03-30 21:41:36 -0500

    Push to include database and database helper class

---
+++ ReggylertTool/src/applicationReggylertTool/SqliteHelper.java
@@ -0,0 +1,96 @@
+package applicationReggylertTool;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class SqliteHelper {
+	private String dbname = "";
+
+	public SqliteHelper(String dbname) {
+		this.dbname = dbname;
+	}
+	
+	public Connection getConnection() {
+		Connection conn = null;
+		try {
+			String url = "jdbc:sqlite:" + this.getResource(this.dbname);
+
+			conn = DriverManager.getConnection(url);
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+		return conn;
+	}
+	
+	public void closeConnection(Connection conn) {
+		try {
+			if(conn != null) {
+				conn.close();
+			}
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public void execute(String sql) throws SQLException {
+		Connection conn = null;
+		try {
+			conn = this.getConnection();
+			Statement stmt = conn.createStatement();
+
+			stmt.execute(sql);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (conn != null) {
+				conn.close();
+			}
+		}
+	}
+
+	public ResultSet executeQuery(Connection conn, String sql) {
+
+		ResultSet rs = null;
+		try {
+			Statement stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+		return rs;
+	}
+
+	public ArrayList<String> getColumnNames(ResultSet rs) {
+		ArrayList<String> al = new ArrayList<String>();
+
+		try {
+
+			ResultSetMetaData rsMetaData;
+			rsMetaData = rs.getMetaData();
+
+			// Retrieving the list of column names
+			int count = rsMetaData.getColumnCount();
+			for (int i = 1; i <= count; i++) {
+				al.add(rsMetaData.getColumnName(i));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return al;
+	}
+
+	private String getResource(String resource) {
+		return getClass().getResource(resource).toString();
+	}
+}
--- ReggylertTool/src/module-info.java
+++ ReggylertTool/src/module-info.java
@@ -1,5 +1,6 @@
 module ReggylertTool {
 	requires javafx.controls;
+	requires java.sql;
 	
 	opens applicationReggylertTool to javafx.graphics, javafx.fxml;
 }
