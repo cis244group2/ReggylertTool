959c7eb167dd5c8d013f61e8ad109d9bf3e88e91
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2022-04-06 21:40:36 -0500

    Email notifications skeleton

--- ReggylertTool_V2/src/applicationReggylerTool_V2/SearchNotification_RT.java
+++ ReggylertTool_V2/src/applicationReggylerTool_V2/SearchNotification_RT.java
@@ -1,10 +1,52 @@
 package applicationReggylerTool_V2;
 
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+
 public class SearchNotification_RT {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+   public static void main(String [] args) {    
+      // Recipient's email ID needs to be mentioned.
+      String to = "abcd@gmail.com";
 
-	}
+      // Sender's email ID needs to be mentioned
+      String from = "web@gmail.com";
 
+      // Assuming you are sending email from localhost
+      String host = "localhost";
+
+      // Get system properties
+      Properties properties = System.getProperties();
+
+      // Setup mail server
+      properties.setProperty("mail.smtp.host", host);
+
+      // Get the default Session object.
+      Session session = Session.getDefaultInstance(properties);
+
+      try {
+         // Create a default MimeMessage object.
+         MimeMessage message = new MimeMessage(session);
+
+         // Set From: header field of the header.
+         message.setFrom(new InternetAddress(from));
+
+         // Set To: header field of the header.
+         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+         // Set Subject: header field
+         message.setSubject("Reggylert: Update");
+
+         // Now set the actual message
+         message.setText("Here are the sub");
+
+         // Send message
+         Transport.send(message);
+         System.out.println("Sent message successfully.");
+      } catch (MessagingException mex) {
+         mex.printStackTrace();
+      }
+   }
 }
