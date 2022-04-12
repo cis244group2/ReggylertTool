9d76841bd81d52f71533fd554c6e53050a0cd639
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2022-04-12 18:42:17 -0500

    Updated notification email

--- ReggylertTool_V2/src/applicationReggylerTool_V2/SearchNotification_RT.java
+++ ReggylertTool_V2/src/applicationReggylerTool_V2/SearchNotification_RT.java
@@ -1,52 +1,56 @@
-package applicationReggylerTool_V2;
+// Java program to send email
 
 import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
 import javax.activation.*;
+import javax.mail.Session;
+import javax.mail.Transport;
+
 
 public class SearchNotification_RT {
+public static void main(String [] args) {
+	// email ID of Recipient.
+	String recipient = "recipient@gmail.com";
 
-   public static void main(String [] args) {    
-      // Recipient's email ID needs to be mentioned.
-      String to = "abcd@gmail.com";
+	// email ID of Sender.
+	String sender = "cis244group2@gmail.com";
 
-      // Sender's email ID needs to be mentioned
-      String from = "web@gmail.com";
+	// using host as localhost
+	String host = "127.0.0.1";
 
-      // Assuming you are sending email from localhost
-      String host = "localhost";
+	// Getting system properties
+	Properties properties = System.getProperties();
 
-      // Get system properties
-      Properties properties = System.getProperties();
+	// Setting up mail server
+	properties.setProperty("mail.smtp.host", host);
 
-      // Setup mail server
-      properties.setProperty("mail.smtp.host", host);
+	// creating session object to get properties
+	Session session = Session.getDefaultInstance(properties);
 
-      // Get the default Session object.
-      Session session = Session.getDefaultInstance(properties);
+	try
+	{
+		// MimeMessage object.
+		MimeMessage message = new MimeMessage(session);
 
-      try {
-         // Create a default MimeMessage object.
-         MimeMessage message = new MimeMessage(session);
+		// Set From Field: adding senders email to from field.
+		message.setFrom(new InternetAddress(sender));
 
-         // Set From: header field of the header.
-         message.setFrom(new InternetAddress(from));
+		// Set To Field: adding recipient's email to from field.
+		message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
 
-         // Set To: header field of the header.
-         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+		// Set Subject: subject of the email
+		message.setSubject("Reggylert Notifications");
 
-         // Set Subject: header field
-         message.setSubject("Reggylert: Update");
+		// set body of the email.
+		message.setText("Please see the updates that you have subscribed to through your notification settings.");
 
-         // Now set the actual message
-         message.setText("Here are the sub");
-
-         // Send message
-         Transport.send(message);
-         System.out.println("Sent message successfully.");
-      } catch (MessagingException mex) {
-         mex.printStackTrace();
-      }
-   }
+		// Send email.
+		Transport.send(message);
+		System.out.println("Mail successfully sent");
+	}
+	catch (MessagingException mex)
+	{
+		mex.printStackTrace();
+	}
 }
