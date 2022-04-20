972351b817b5ee42a5305f5a648a7e2824e7d5b7
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2022-04-19 23:10:18 -0500

    Sending emails. Update the password section after you pull this

--- ReggylertTool_V2/src/applicationReggylerTool_V2/SearchNotification_RT.java
+++ ReggylertTool_V2/src/applicationReggylerTool_V2/SearchNotification_RT.java
@@ -3,55 +3,66 @@ package applicationReggylerTool_V2;
 import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
-//import javax.activation.*;
 import javax.mail.Session;
 import javax.mail.Transport;
 
-
 public class SearchNotification_RT {
-public static void main(String [] args) {
-	// email ID of Recipient.
-	String recipient = "recipient@gmail.com";
+	private static String USERNAME = "cis244group2@gmail.com"; 
+	private static String PASS = "NOTTHEPASSWORD"; //put in password here
+	private static String RECIPIENT1 = "jnguyen52@student.ccc.edu"; //field_recipientEmail;
+	private static String RECIPIENT2 = "garias1@student.ccc.edu"; //field_recipientEmail;
+	private static String RECIPIENT3 = "jhector1@student.ccc.edu"; //field_recipientEmail;
+	private static String RECIPIENT4 = "poliveira1@student.ccc.edu"; //field_recipientEmail;
 
-	// email ID of Sender.
-	String sender = "cis244group2@gmail.com";
+	
+  public static void main(String[] args) {
+      String from = USERNAME;
+      String login = PASS;
+      String[] to = { RECIPIENT1, RECIPIENT2, RECIPIENT3, RECIPIENT4 }; //recipient email addresses, can leave it to RECIPIENT1
+      String subject = "Reggylert Notifications";
+      String body = "Hello! Here are your Reggylert notifications you have subscribed to."; 
+      // field_recipientFirstName + " " + field_recipientFirstName + "'s Notifications";
+      sendEmail(from, login, to, subject, body);
+  }
 
-	// using host as localhost
-	String host = "127.0.0.1";
+  private static void sendEmail(String from, String pass, String[] to, String subject, String body) {
+      Properties props = System.getProperties();
+      String host = "smtp.gmail.com";
+      props.put("mail.smtp.starttls.enable", "true");
+      props.put("mail.smtp.host", host);
+      props.put("mail.smtp.user", from);
+      props.put("mail.smtp.password", pass);
+      props.put("mail.smtp.port", "587");
+      props.put("mail.smtp.auth", "true");
 
-	// Getting system properties
-	Properties properties = System.getProperties();
+      Session session = Session.getDefaultInstance(props);
+      MimeMessage message = new MimeMessage(session);
 
-	// Setting up mail server
-	properties.setProperty("mail.smtp.host", host);
+      try {
+          message.setFrom(new InternetAddress(from));
+          InternetAddress[] toAddress = new InternetAddress[to.length];
 
-	// creating session object to get properties
-	Session session = Session.getDefaultInstance(properties);
+          // To get the array of addresses
+          for( int i = 0; i < to.length; i++ ) {
+              toAddress[i] = new InternetAddress(to[i]);
+          }
 
-	try
-	{
-		// MimeMessage object.
-		MimeMessage message = new MimeMessage(session);
+          for( int i = 0; i < toAddress.length; i++) {
+              message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+          }
 
-		// Set From Field: adding senders email to from field.
-		message.setFrom(new InternetAddress(sender));
-
-		// Set To Field: adding recipient's email to from field.
-		message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
-
-		// Set Subject: subject of the email
-		message.setSubject("Reggylert Notifications");
-
-		// set body of the email.
-		message.setText("Please see the updates that you have subscribed to through your notification settings.");
-
-		// Send email.
-		Transport.send(message);
-		System.out.println("Mail successfully sent");
-	}
-	catch (MessagingException mex)
-	{
-		mex.printStackTrace();
-	}
-}
+          message.setSubject(subject);
+          message.setText(body);
+          Transport transport = session.getTransport("smtp");
+          transport.connect(host, from, pass);
+          transport.sendMessage(message, message.getAllRecipients());
+          transport.close();
+      }
+      catch (AddressException ae) {
+          ae.printStackTrace();
+      }
+      catch (MessagingException me) {
+          me.printStackTrace();
+      }
+  }
 }
