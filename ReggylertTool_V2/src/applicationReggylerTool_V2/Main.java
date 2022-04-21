37a0ee6edb5ac597e136ca27081a9bbe91eb94fa
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2022-04-20 20:39:04 -0500

    setting title

--- ReggylertTool_V2/src/applicationReggylerTool_V2/Main.java
+++ ReggylertTool_V2/src/applicationReggylerTool_V2/Main.java
@@ -27,6 +27,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root,755,565);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Reggylert");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
