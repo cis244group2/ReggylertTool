7a9b1ab87604c8896e3bf0a50b302d0685a6dbda
paulo <paulo@192.168.0.13>
2022-04-23 00:46:32 -0500

    Adding code for initial keyword search

--- ReggylertTool_V2/src/applicationReggylerTool_V2/KeywordSearch_RT.java
+++ ReggylertTool_V2/src/applicationReggylerTool_V2/KeywordSearch_RT.java
@@ -1,10 +1,43 @@
 package applicationReggylerTool_V2;
 
-public class KeywordSearch_RT {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class KeywordSearch_RT 
+{
+   public static void main(String[] args) throws IOException 
+   {
+      File f1=new File("input.txt"); //Creation of File Descriptor for input file
+      String[] words=null;  //Initialize the word Array - ALIGN THIS TO WORDS ENTERED BY USER
+      FileReader fr = new FileReader(f1);  //Creation of File Reader object
+      BufferedReader br = new BufferedReader(fr); //Creation of BufferedReader object
+      String s;     
+      String input="Java";   // Input word to be searched
+      int count=0;   
+      while((s=br.readLine())!=null)   //Reading Content from the file
+      {
+         words=s.split(" ");  //Split the word using space
+          for (String word : words) 
+          {
+                 if (word.equals(input))   //Search for the given word
+                 {
+                   count++;    //If Present increase the count by one
+                   				
+                 }
+          }
+      }
+      if(count!=0)  //Check for count not equal to zero
+      {
+         System.out.println("The given keyword is present for "+count+ " Times in the file");
+         // ADD CODE FOR ENTERING SEARCH FOUND INTO DATABASE AND SENDING NOTIFICATION HERE
+      }
+      else
+      {
+         System.out.println("The given keyword is not present in the file");
+      }
+      
+         fr.close();
+   }
 }
