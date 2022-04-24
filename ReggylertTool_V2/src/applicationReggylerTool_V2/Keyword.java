b67b1316310934c0732b37b25569f66fe0690b69
paulo <paulo@192.168.0.13>
2022-04-23 21:14:56 -0500

    keyword class added

---
+++ ReggylertTool_V2/src/applicationReggylerTool_V2/Keyword.java
@@ -0,0 +1,75 @@
+package applicationReggylerTool_V2;
+
+import java.io.Serializable;
+
+public class Keyword implements Serializable {
+	
+	private String keyword;
+	private String keywordID;
+	private String priorityRating;
+	private String controlStd;
+	private String dateModified;
+	private String status;
+	
+	public Keyword(String keyword, String keywordID, String priorityRating, String controlStd, String dateModified,
+			String status) {
+		this.keyword = keyword;
+		this.keywordID = keywordID;
+		this.priorityRating = priorityRating;
+		this.controlStd = controlStd;
+		this.dateModified = dateModified;
+		this.status = status;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+	public String getKeywordID() {
+		return keywordID;
+	}
+
+	public void setKeywordID(String keywordID) {
+		this.keywordID = keywordID;
+	}
+
+	public String getPriorityRating() {
+		return priorityRating;
+	}
+
+	public void setPriorityRating(String priorityRating) {
+		this.priorityRating = priorityRating;
+	}
+
+	public String getControlStd() {
+		return controlStd;
+	}
+
+	public void setControlStd(String controlStd) {
+		this.controlStd = controlStd;
+	}
+
+	public String getDateModified() {
+		return dateModified;
+	}
+
+	public void setDateModified(String dateModified) {
+		this.dateModified = dateModified;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+	
+
+}
