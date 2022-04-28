8013f997613d7c16b795d74275a9dbe659e4ab9b
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2022-04-27 19:27:03 -0500

    updated verbiage of email template

--- ReggylertTool_V2/src/applicationReggylerTool_V2/SearchNotification_RT.java
+++ ReggylertTool_V2/src/applicationReggylerTool_V2/SearchNotification_RT.java
@@ -2,17 +2,19 @@ package applicationReggylerTool_V2;
 
 import java.util.*;
 
-public class SearchNotification_RT extends Email {	
+public class SearchNotification_RT extends Email {	 
 	public static void main(String[] args) {
-		Date date = new Date();
+		Date date = new Date(); 
 		
 		Email notificationEmail = new Email();
 		String from = notificationEmail.getUsername();
 		String login = notificationEmail.getPass();
-		String[] to = {"jnguyen52@student.ccc.edu"}; //add recipients here
+		String[] to = {"jnguyen52@student.ccc.edu"}; //add recipients
 		String subject = "Reggylert Search Notification";
+		
 		String body = "Date: " + date + 
-    		  "\n\nHello! Here are your Reggylert notifications you have subscribed to."; 
+    		  "\n\nHello!\n\nA search has been found. Please refer to the below:\n\n"
+    		  + "Best regards,\nReggylert Tool";
 		sendEmail(from, login, to, subject, body);
   }
 }
