d38593d1e5bccc93f42a9fa329616d3d5cffba2b
paulo <paulo@192.168.0.13>
2022-05-01 16:33:43 -0500

    Loading recipient into database

--- ReggylertTool_V2/src/applicationReggylerTool_V2/Controller_Record_Keyword.java
+++ ReggylertTool_V2/src/applicationReggylerTool_V2/Controller_Record_Keyword.java
@@ -38,7 +38,7 @@ public class Controller_Record_Keyword {
     @FXML
     void action_SubmitKeyword(ActionEvent event) throws IOException {
     	
-    	this.save_record();
+    	this.save_Keyrecord();
     	  	
     	//set up the call back event for event to update table
     	
@@ -52,7 +52,7 @@ public class Controller_Record_Keyword {
     	  	
     }
     
-    void save_record() {
+    void save_Keyrecord() {
     	SQLhelper sqlHelper = new SQLhelper("Database_RT.db"); 
     	String sql = String.format("INSERT INTO keywordinventory (keyword, keywordid, priorityrating, controlstandardid, datemodified, status) VALUES('%s', '%s', '%s', '%s', '%s', '%s');",
     			this.field_Keyword.getText(), this.field_KeywordID.getText(), this.field_PriorityTierRating.getText(), this.field_ControlStandardID.getText(), this.field_DateModified.getText(), this.field_KeywordStatus.getText());
--- ReggylertTool_V2/src/applicationReggylerTool_V2/Controller_Record_NotificationRecipient.java
+++ ReggylertTool_V2/src/applicationReggylerTool_V2/Controller_Record_NotificationRecipient.java
@@ -6,30 +6,28 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 
 public class Controller_Record_NotificationRecipient {
 
-    @FXML
+	@FXML
     private Button button_recipientSubmit;
 
     @FXML
     private TextField field_recipientEmail;
 
     @FXML
-    private TextField field_recipientFirstName;
+    private TextField field_recipientName;
 
     @FXML
-    private TextField field_recipientLastName;
+    private TextField field_recipientStatus;
 
     @FXML
-    private ChoiceBox<?> field_recipientStatus;
-
-    @FXML
-    void button_recipientFirstName(ActionEvent event) throws IOException {
+    void button_recipientSubmit(ActionEvent event) throws IOException {
+    	
+    	this.save_Recipientrecord();
     	
     	try {
     		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -39,6 +37,21 @@ public class Controller_Record_NotificationRecipient {
 			e.printStackTrace();
 		}
 
+    }    
+    
+    void save_Recipientrecord() {
+    	SQLhelper sqlHelper = new SQLhelper("Database_RT.db"); 
+    	String sql = String.format("INSERT INTO recipientinventory (name, email, status) VALUES('%s', '%s', '%s');",
+    			this.field_recipientName.getText(), this.field_recipientEmail.getText(), this.field_recipientStatus.getText());
+    	
+    	try {
+    		sqlHelper.execute(sql);
+    		System.out.println("Recorded was added successfully");
+    	}
+    	catch(Exception ex) {
+    		System.out.println("Error!");
+    	}
+    	
     }
     
     
